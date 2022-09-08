f4854d0180dc5c842eb937b8d8e3a7a46c1aa182
qvarkk <xumdes@gmail.com>
2022-09-08 17:48:39 +0500

    Added win check

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const gameBoard = {
   changeGameState: function() {
     if (gameBoard.gameState === 'x') {
       gameBoard.gameState = 'o';
-    } else {
+    } else if (gameBoard.gameState === 'o') {
       gameBoard.gameState = 'x';
     }
   }
@@ -34,10 +34,32 @@ const gameBoard = {
 // idk if I can fit this in the object, better this way
 for (let i = 0; i < 9; i++) {
   gameBoard.boardTiles[i].addEventListener('click', () => {
-    if (gameBoard.boardState[i] === '') {
+    if (gameBoard.boardState[i] === '' && gameBoard.gameState !== 'over') {
       gameBoard.boardState[i] = gameBoard.gameState;
       gameBoard.gridUpdate();
+      gameController.checkWin();
       gameBoard.changeGameState();
     }
   });
+}
+
+const gameController = {
+  winConditions: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ],
+  checkWin: function() {
+    for (let i = 0; i < 8; i++) {
+      if (gameBoard.boardState[this.winConditions[i][0]] !== '' && gameBoard.boardState[this.winConditions[i][0]] === gameBoard.boardState[this.winConditions[i][1]] && gameBoard.boardState[this.winConditions[i][0]] === gameBoard.boardState[this.winConditions[i][2]]) {
+        gameBoard.gameState = 'over';
+        console.log('win');
+      }
+    }
+  }
 }
